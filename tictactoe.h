3ef976e6819f2a4318b2b6925c7669ffb8447c26
Devano Jonathan <67155803+devoob@users.noreply.github.com>
2023-05-03 02:39:43 +0800

    Update tictactoe.h

--- tictactoe.h
+++ tictactoe.h
@@ -5,10 +5,10 @@
 
 const int SIZE = 3;
 
-void printBoard(const std::vector<std::vector<char>>& board);
-bool makeHumanMove(std::vector<std::vector<char>>& board);
-bool makeComputerMove(std::vector<std::vector<char>>& board);
-bool checkWin(const std::vector<std::vector<char>>& board, char player);
-bool isDraw(const std::vector<std::vector<char>>& board);
+void printBoard(const std::vector<std::vector<char> >& board);
+bool makeHumanMove(std::vector<std::vector<char> >& board);
+bool makeComputerMove(std::vector<std::vector<char> >& board);
+bool checkWin(const std::vector<std::vector<char> >& board, char player);
+bool isDraw(const std::vector<std::vector<char> >& board);
 
 #endif // TICTACTOE_H
