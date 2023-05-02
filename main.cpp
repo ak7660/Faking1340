5132d57d4567485a15a6ff5f115514996c0142fc
Ahmad Khan <120759021+ak7660@users.noreply.github.com>
2023-05-03 00:20:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <cctype>
+#include "mazes.h"
 
+// Encryption and decryption functions are omitted in this example
+// as they require a separate implementation using C++ cryptography libraries.
+
+// Player ranking function
+void player_ranking();
+
+// Initiate function
+int initiate();
+
+// Story function
+void story();
+
+int main() {
+    story();
+    int difficulty = initiate();
+    if (difficulty == 0) {
+        return 0;
+    }
+
+    // Add game logic here
+
+    return 0;
+}
+
+void player_ranking() {
+    // Implement player ranking logic here
+}
+
+int initiate() {
+    bool invalid = false;
+    while (true) {
+        std::cout << "\033[H\033[J";
+        if (invalid) {
+            std::cout << "->Invalid input, try again! ";
+        }
+        // Add logo and other prints here
+
+        player_ranking();
+        std::cout << "=====================================================================\n";
+        std::cout << "\n-->Enter (Q) to exit<--\n\n";
+        while (true) {
+            std::string user_input;
+            std::cin >> user_input;
+            if (!user_input.empty() && std::isdigit(user_input[0]) && 0 < std::stoi(user_input) && std::stoi(user_input) < 4) {
+                return std::stoi(user_input);
+            } else if (user_input == "Q") {
+                return 0;
+            } else {
+                invalid = true;
+                break;
+            }
+        }
+    }
+}
+
+void story() {
+    // Add story logic here
+}
