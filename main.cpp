69134d5b24a4fee76b637e46ebeff5b153dc1912
Nick14848 <82718916+Nick14848@users.noreply.github.com>
2023-05-03 00:41:48 +0800

    Story part

--- main.cpp
+++ main.cpp
@@ -63,5 +63,62 @@ int initiate() {
 }
 
 void story() {
-    // Add story logic here
+    system("clear");
+    string x;
+    cout << "Press ENTER to read the story & rules OR (E) start the game directly: ";
+    getline(cin, x);
+    system("clear");
+
+    string text[] = {
+        "Background story: ",
+        "",
+        "->In 1986, a nuclear leak accident occured nearby your hometown.",
+        "->Millions of innocent villagers were exposed to highly radioactive chemicals.",
+        "->You need to escape this Ghost Town as soon as possible!"
+    };
+
+    string image[] = {
+        "+-----------+/__\  _      / \      /   /     ( )| | |                     |   ( )_)| | (_)_)    ",
+        "|_|_|_|_|_|_||__| / \    /___\    /   /    (__)_) | | NUCLEAR POWER PLANT |  (__)_)| | | |     ",
+        "|_|_|_|_|_|_|    /   \   |   |   /   /     |  | |_| |                     |   |  | |_| | |    ",
+        "|_|_|_|_|_|_|   /_____\  |___|  /   /      |  |     |                     |   |  |     | |     ",
+        "|_|_|_|_|_|_|   |     |        /   /       |  |     |       _______       |   |  |     | |     ",
+        "|   |   |   |   |  _  |       /   /        |  |     |      |       |      |   |  |     |_|     ",
+        "|___________|   |_|_|_|      /   /         |__|     |______|_______|______|   |__|                  "
+    };
+
+    if (x != "E") {
+        for (const string &line : image) {
+            cout << line << endl;
+        }
+        cout << "============================================================================================" << endl;
+        for (const string &line : text) {
+            for (char ch : line) {
+                cout << ch;
+                cout.flush();
+                usleep(40000);
+            }
+            cout << endl;
+        }
+        cout << endl;
+        cout << "Press ENTER to continue: ";
+        getline(cin, x);
+        system("clear");
+        cout << "Rules: " << endl;
+        cout << endl;
+        cout << "->Enter (W, A, S, D) to move and find a way to the exit (E) in the maze, each move will be recorded as one step." << endl;
+        cout << "->Items in the maze: (*) = Randomly played a game (difficulty of the game depends on the difficulty chose at the beginning)." << endl;
+        cout << "                           If you win your steps will be decreased by 6." << endl;
+        cout << "                           If you gave up or lost you will be teleported back to where you started." << endl;
+        cout << "                     (?) = Random teleport." << endl;
+        cout << "->Depending on the choosen difficulty level, the final score (RADIATION LEVEL) = steps X weight" << endl;
+        cout << "->Difficulty 1: weight = 3, Difficulty 2: weight = 2, Difficulty 3: weight = 1." << endl;
+        cout << "->The lower the RADIATION LEVEL, the higher the rank." << endl;
+        cout << "->The score will only be updated on the ranking system after you reach the exit and if you are a new player OR your RADIATION LEVEL is lower than previous." << endl;
+        cout << "->The goal is to escape the Ghost Town with minimum steps!" << endl;
+        cout << endl;
+        cout << "Press ENTER to continue: ";
+        getline(cin, x);
+    }
+}
 }
