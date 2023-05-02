11e257e9104f7adc01279cb0f83621fc221c0d6a
Devano Jonathan <67155803+devoob@users.noreply.github.com>
2023-05-03 01:02:08 +0800

    Update ticktack.h

--- ticktack.h
+++ ticktack.h
@@ -1,1 +1,14 @@
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
 
+#include <vector>
+
+const int SIZE = 3;
+
+void printBoard(const std::vector<std::vector<char>>& board);
+bool makeHumanMove(std::vector<std::vector<char>>& board);
+bool makeComputerMove(std::vector<std::vector<char>>& board);
+bool checkWin(const std::vector<std::vector<char>>& board, char player);
+bool isDraw(const std::vector<std::vector<char>>& board);
+
+#endif // TICTACTOE_H
