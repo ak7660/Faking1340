ff789ca4667051329c8cc56a94c0f28f8a6b5052
Mario11 <121161385+Mario3xtra11@users.noreply.github.com>
2023-05-03 00:46:32 +0800

    Update tntman.h

--- tntman.h
+++ tntman.h
@@ -1,1 +1,24 @@
+#ifndef TNTMAN_H
+#define TNTMAN_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Hangman
+{
+public:
+    Hangman(int difficulty);
+    bool play();
+
+private:
+    string ans;
+    vector<char> alpans;
+    vector<char> response;
+    int chance;
+
+    char get_user_input(const string &prompt);
+};
+
+#endif
