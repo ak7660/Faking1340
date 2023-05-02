18783f26d2d583c0cf29f780bba8cf2ffe347d45
Mario11 <121161385+Mario3xtra11@users.noreply.github.com>
2023-05-03 00:43:49 +0800

    Update tile.h

--- tile.h
+++ tile.h
@@ -1,1 +1,18 @@
+#ifndef TILE_GAME_H
+#define TILE_GAME_H
 
+#include <vector>
+#include <string>
+
+class TileGame
+{
+public:
+    void run(int lvl);
+
+private:
+    bool tile(int lvl);
+    std::vector<int> movable(const std::vector<int> &board, int row, int col, int pos);
+    void print_board(const std::vector<int> &board, int row, int col);
+};
+
+#endif // tile_h
