5276595b54c9e2e3cf388fcc3b2fa5e9134287e7
Ahmad Khan <120759021+ak7660@users.noreply.github.com>
2023-05-04 03:14:07 +0800

    Update minigame.h

--- Test/minigame.h
+++ Test/minigame.h
@@ -1,1 +1,31 @@
+#ifndef MINIGAME_H
+#define MINIGAME_H
 
+#include <vector>
+#include <functional>
+
+class MiniGame {
+public:
+    MiniGame();
+    bool play_random_game(); // Changed return type to bool
+
+private:
+    std::vector<std::function<bool()>> games; // Updated function signature
+    static bool game1(); // Changed return type to bool
+    static bool game2(); // Changed return type to bool
+    static bool game3(); // Changed return type to bool
+    static bool game4(); // Changed return type to bool
+    static bool game5(); // Changed return type to bool
+    static bool game6(); // Changed return type to bool
+    static bool game7(); // Changed return type to bool
+    static bool game8(); // Changed return type to bool
+    static bool game9(); // Changed return type to bool
+    static bool game10(); // Changed return type to bool
+    static bool game11(); // Changed return type to bool
+    static bool game12(); // Changed return type to bool
+    static bool game13(); // Changed return type to bool
+    static bool game14(); // Changed return type to bool
+    static bool game15(); // Changed return type to bool
+};
+
+#endif // MINIGAME_H
