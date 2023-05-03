837f5625632739ea1d2d19e33bc9fe4c5e2a238b
Ahmad Khan <120759021+ak7660@users.noreply.github.com>
2023-05-04 03:16:57 +0800

    Update maze.h

--- Test/maze.h
+++ Test/maze.h
@@ -1,1 +1,21 @@
+#ifndef MAZE_H
+#define MAZE_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Maze {
+public:
+    Maze(std::istream& input);
+    void print() const;
+    int move_player(char command, bool won_mini_game = false);
+    bool check_win() const;
+    void teleport_to_start();
+
+private:
+    std::vector<std::string> map;
+    int player_x, player_y;
+};
+
+#endif // MAZE_H
